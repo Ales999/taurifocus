9a7f871388a3e18aaedec9f7e3a324b7dd5ae28f
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2026-01-29 02:08:01 +0500

    Configure window

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+//import reactLogo from "./assets/react.svg";
+//import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [text, setText] = useState('')
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+  const addTask = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      console.log(text)
+    }
   }
 
   return (
-    <main className="container">
-      <div className="hover:bg-red-500">
-          <h1 className="text-3xl font-bold underline ">
-            Hello world!
-          </h1>
-      </div>
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+    <input
+      type='text'
+      className='w-150 h-15 px-4 bg-gray-800 text-2xl text-green-600 rounded-sm'
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      onKeyDown={addTask}
+    />
+  )
 }
 
-export default App;
+export default App
