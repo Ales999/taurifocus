399f4643cf82ef79fda56cc52fa09d7f71cbfa3a
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2026-01-29 04:20:53 +0500

    Add exit app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,60 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+// импорт зависимостей
+use std::fs::OpenOptions;
+use std::io::Write;
+
+//use tauri::plugin::{Builder as PluginBuilder, TauriPlugin};
+//use tauri::Runtime;
+
+//#[warn(unused_imports)]
+//use tauri::menu::Menu;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn add_task(text: String) {
+  let mut file = OpenOptions::new()
+    .create(true)
+    .append(true)
+    .open("../tasks.txt")
+    .expect("Ошибка при открытии файла");
+
+  writeln!(file, "{}", text).expect("Ошибка при записи файла");
 }
+
+#[tauri::command]
+fn exit_app() {
+    std::process::exit(0);
+}
+/*
+// Регистрация команд через плагин (обязательно для Tauri 2.x)
+fn commands_plugin<R: Runtime>() -> TauriPlugin<R> {
+    PluginBuilder::new("commands")
+        .invoke_handler(tauri::generate_handler![add_task])
+        .invoke_handler(tauri::generate_handler![exit_app]) // Регистрируем команду завершения приложения.
+        .build()
+}
+*/
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+
+    let context = tauri::generate_context!();
+    //.plugin(tauri_plugin_opener::init())
+    //.plugin(commands_plugin()) // Замена invoke_handler на плагин
+    //.menu(|app| Menu::default(app)) // Упрощённое API меню
+
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .invoke_handler(tauri::generate_handler![add_task, exit_app])
+        .run(context)
+        .expect(&format!("Error when running application"));
+
+        //.expect("Ошибка при запуске приложения");
+
+
+        //.invoke_handler(tauri::generate_handler![greet])
+        //.run(tauri::generate_context!())
+        //.expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 //import reactLogo from "./assets/react.svg";
-//import { invoke } from "@tauri-apps/api/core";
+import { invoke } from "@tauri-apps/api/core";
+//import { invoke, process } from '@tauri-apps/api'
+
 import "./App.css";
 
 function App() {
   const [text, setText] = useState('')
 
-  const addTask = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') {
-      console.log(text)
-    }
+  const addTask = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    //if ( e && !isNaN(+text)) return;
+
+    switch (e.key) {
+    // при нажатии `Enter` вызываем `add_task` с текстом заметки
+    case 'Enter':
+      try {
+        await invoke('add_task', { text })
+        setText('')
+      } catch (e) {
+        console.error(e)
+      }
+      break
+    // при нажатии `Esc` завершаем процесс
+    case 'Escape':
+      await invoke('exit_app')
+      return
+    default:
+      return
+  }
+
   }
 
   return (
