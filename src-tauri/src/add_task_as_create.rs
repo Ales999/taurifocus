3308d1588c37276994194d80234166064bf3c9d7
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2026-02-09 23:56:43 +0500

    Reafactor reating new file  for tasks

--- src-tauri/src/add_task_as_create.rs
+++ src-tauri/src/add_task_as_create.rs
@@ -1,29 +1,15 @@
 // src-tauri/src/add_task_as_create.rs
 
 use std::fs::OpenOptions;
+use std::io::Write;
 use std::path::Path;
-use std::io::{Write};
 
 // Создаем файл и записываем в него строку.
 pub fn created_line<P: AsRef<Path>>(path: P, line: &str) -> std::io::Result<()> {
-    let file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(&path);
+    // Создаем новый файл.
+    let mut file = OpenOptions::new().write(true).create(true).open(&path)?;
 
-    match file {
-        Ok(mut f) => {
-            // Проверяем результат записи
-            writeln!(f, "{}", line)?;
-            // Проверяем результат сброса буфера
-            f.flush()?;
+    writeln!(file, "{}", line)?; // Проверяем результат записи
 
-            Ok(())
-        }
-        Err(e) => {
-            #[cfg(dev)]
-            println!("Ошибка при открытии файла: {}", e);
-            Err(e)
-        }
-    }
+    Ok(())
 }
